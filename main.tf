65928553826d19e00bcbe207da00aeb8983fe042
srikaanth <srikaanth62@gmail.com>
2023-06-03 13:54:25 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = merge(var.tags, {Name = "${var.env}-vpc"})
+}
+
+module "subnets" {
+  source = "./subnets"
+  for_each = var.subnets
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value["cidr_block"]
+  name = each.value["name"]
+  azs = each.value["azs"]
+  tags = var.tags
+  env = var.env
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, {Name = "${var.env}-igw"})
+}
+
+resource "aws_eip" "ngw" {
+  count = length(var.subnets["public"].cidr_block)
+  vpc = true
+  tags = merge(var.tags, {Name = "${var.env}-ngw"})
+}
+
+resource "aws_nat_gateway" "example" {
+  count = length(var.subnets["public"].cidr_block)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
+  tags = merge(var.tags, {Name = "${var.env}-ngw"})
+
+}
